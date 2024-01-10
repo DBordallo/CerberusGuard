10bcea3dd4fd2aa54274aca10f491a9bf4918a07
Jassed <jassedgmartinez@gmail.com>
2024-01-10 12:11:07 +0100

    fix(routes)route-login-fixed

--- client/src/router/routes.jsx
+++ client/src/router/routes.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Root from "./Root";
 import ErrorElement from "../pages/errorElement/ErrorElement";
 import Home from "../pages/home/Home";
-import { Login } from "../pages/login/Login";
+import Login from "../pages/login/Login"
 import Register from "../pages/register/Register";
 
 const router = createBrowserRouter([
