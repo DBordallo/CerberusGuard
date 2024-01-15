bd1f89076d81420639d06899a6ccd6130ab19489
Diego Bordallo <bordallo.d@gmail.com>
2024-01-15 15:38:09 +0100

    fix(HeaderLogo/HeaderLogoText)center logo & router addaccount

--- client/src/router/routes.jsx
+++ client/src/router/routes.jsx
@@ -31,7 +31,7 @@ const router = createBrowserRouter([
                 ),
             },
             {
-                path: "/addacount",
+                path: "/addaccount",
                 element: <NewPassword />,
             },
         ]
