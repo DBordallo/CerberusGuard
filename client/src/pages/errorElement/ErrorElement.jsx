6c689c1d64236d9167a056d153cbfb573aa24265
Diego Bordallo <bordallo.d@gmail.com>
2024-02-02 12:36:01 +0100

    feat(404page)add styles

--- client/src/pages/errorElement/ErrorElement.jsx
+++ client/src/pages/errorElement/ErrorElement.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import error404dog from '../../assets/error404dog.gif';
+import "./ErrorElement.css"
 
 function ErrorElement(){
     return(
-        <div>
-            volver al login <Link to="/">aquí</Link>
+        <div className="NotFound">
+         <img className ="errorImg"  src={error404dog} alt="error 404" />
+         <div className="errorText">¡Oops! Este perro se escapó y decidió hacer travesuras en la ruta que estabas explorando. Mejor retrocede a la página principal <Link to="/">aquí</Link>
+         </div>
         </div>
     )
 }
