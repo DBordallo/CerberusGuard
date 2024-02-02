eb7e871f2a2ea3193ba202e642f1f25693ee56eb
Diego Bordallo <bordallo.d@gmail.com>
2024-02-02 12:50:41 +0100

    fix(404)fix styles

--- client/src/pages/errorElement/ErrorElement.jsx
+++ client/src/pages/errorElement/ErrorElement.jsx
@@ -7,7 +7,7 @@ function ErrorElement(){
     return(
         <div className="NotFound">
          <img className ="errorImg"  src={error404dog} alt="error 404" />
-         <div className="errorText">¡Oops! Este perro se escapó y decidió hacer travesuras en la ruta que estabas explorando. Mejor retrocede a la página principal <Link to="/">aquí</Link>
+         <div className="errorText parp">¡Oops!This dog escaped and decided to misbehave on the route you were exploring. It's better to backtrack to the main page <Link to="/">here</Link>.
          </div>
         </div>
     )
