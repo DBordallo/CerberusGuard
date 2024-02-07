6166a76fc8e715444342d5b8d4a02c482e5a66d3
Diego Bordallo <bordallo.d@gmail.com>
2024-02-07 13:50:39 +0100

    modify main.js

--- client/src/main.js
+++ client/src/main.js
@@ -1,11 +1,11 @@
-"use strict";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { RouterProvider } from 'react-router-dom';
+import routes from './router/routes';
 
-var _react = _interopRequireDefault(require("react"));
-var _client = _interopRequireDefault(require("react-dom/client"));
-require("./index.css");
-var _reactRouterDom = require("react-router-dom");
-var _routes = _interopRequireDefault(require("./router/routes"));
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-_client["default"].createRoot(document.getElementById('root')).render( /*#__PURE__*/_react["default"].createElement(_react["default"].Fragment, null, /*#__PURE__*/_react["default"].createElement(_reactRouterDom.RouterProvider, {
-  router: _routes["default"]
-})));
+createRoot(document.getElementById('root')).render(
+  <React.Fragment>
+    <RouterProvider router={routes} />
+  </React.Fragment>
+);
